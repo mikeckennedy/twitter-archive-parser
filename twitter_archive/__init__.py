756c7f5ddedb49036712593fa939c8244ae9eb96
Michael Kennedy <mikeckennedy@gmail.com>
2022-11-14 21:26:37 -0800

    Add pipx installable version.

--- twitter_archive/__init__.py
+++ twitter_archive/__init__.py
@@ -1,7 +1,7 @@
-__version__ = "0.0.1"
-
 from . import download_better_images
 from . import parser
+
+__version__ = "0.0.1"
 
 parse_export = parser.main
 download_images = download_better_images.main
